11caa197b79b7ff1eca9cd99e37d2365f49b1c37
David Martinez <contacto@davidmartinez.net>
2015-08-22 00:05:43 +0200

    Excluded .env on builds

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ VERSION=`cat composer.json | grep 'version' | awk '{print $2}' \
        | sed -e 's/"//g' | sed -e 's/,//'`
 
 tar czvf "./build/wordpress-plugin-feed-$VERSION.tar.gz" ./ \
-    --exclude=.git --exclude=.idea --exclude=nbproject \
+    --exclude=.env --exclude=.git --exclude=.idea --exclude=nbproject \
     --exclude=build* --exclude=cache/zfcache-* --exclude=composer.lock
