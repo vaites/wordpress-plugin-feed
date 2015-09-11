baa7b6d0c7c5053517cb2da2518dd41d4de28325
David Martinez <contacto@davidmartinez.net>
2015-09-11 18:20:40 +0200

    Enhanced build script

--- build.sh
+++ build.sh
@@ -3,8 +3,13 @@
 VERSION=`cat composer.json | grep 'version' | awk '{print $2}' \
        | sed -e 's/"//g' | sed -e 's/,//'`
 
-composer update
+BUILD="./build/wordpress-plugin-feed-$VERSION.tar.gz"
 
-tar czvf "./build/wordpress-plugin-feed-$VERSION.tar.gz" ./ \
-    --exclude=.env --exclude=.git --exclude=.idea --exclude=nbproject \
-    --exclude=build* --exclude=cache/zfcache-* --exclude=composer.lock
+if [ ! -f $BUILD ]; then
+    composer update
+
+    tar czvf $BUILD ./ --exclude=.env --exclude=.git --exclude=.idea --exclude=nbproject \
+                       --exclude=build* --exclude=cache/zfcache-* --exclude=composer.lock
+else
+    echo "$BUILD already exists..."
+fi
