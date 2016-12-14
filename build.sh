f45c258a14999f6c2314a43a24b65df8c889bc83
David Martinez <contacto@davidmartinez.net>
2016-12-14 01:11:22 +0100

    Fixed package builder

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ BUILD="./build/wordpress-plugin-feed-$VERSION.tar.gz"
 if [ ! -f $BUILD ]; then
     composer update
 
-    tar czvf $BUILD ./ --exclude=.env --exclude=.git --exclude=.idea --exclude=nbproject \
-                       --exclude=reports --exclude=build* --exclude=cache/zfcache-* --exclude=composer.lock
+    tar czvf $BUILD --exclude=.env --exclude=.git --exclude=.idea  --exclude=reports --exclude=build* \
+                    --exclude=cache/zfcache-* --exclude=composer.lock ./
 else
     echo "$BUILD already exists..."
 fi
