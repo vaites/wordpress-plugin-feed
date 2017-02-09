a5eba52c9da1923afbafddb0c28dc0811b88a167
David Martinez <contacto@davidmartinez.net>
2017-02-09 10:04:55 +0100

    Avoid composer update on build script

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ VERSION=`git tag --sort version:refname | tail -n 1 | sed -e 's/v//'`
 BUILD="./build/wordpress-plugin-feed-$VERSION.tar.gz"
 
 if [ ! -f $BUILD ]; then
-    composer update
-
     tar czvf $BUILD --exclude=.env --exclude=.git --exclude=.idea  --exclude=reports --exclude=build* \
                     --exclude=cache/zfcache-* --exclude=composer.lock ./
 else
